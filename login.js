6e9226bb104cdc7776e21594176349d552bf59ea
DogusGok <dgsgk11@gmail.com>
2022-12-08 09:33:01 +0300

    updated from dgs

--- login.js
+++ login.js
@@ -18,15 +18,7 @@ const connection = mysql.createConnection({
 app.get("/", function (req, res) {
   res.render("login");
 });
-app.get("/user", function (req, res) {
-  res.render("user");
-});
-app.get("/company", function (req, res) {
-  res.render("company");
-});
-app.get("/admin", function (req, res) {
-  res.render("admin");
-});
+
 app.post("/", function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
@@ -35,16 +27,33 @@ app.post("/", function (req, res) {
     if (err) throw err;
     console.log("Connected!");
     connection.query(
-      "SELECT username, password,user_type FROM user WHERE username = ? AND password = ?",
-      [username, password],
-      function (err, results) {
-        if (results[0].user_type == "user") {
-          res.redirect("/user");
-        } else throw err;
+      "SELECT username, password,account_type FROM accounts WHERE username =" +
+        connection.escape(username) +
+        "AND password =" +
+        connection.escape(password),
+      function (err, results, rows) {
+        if (err) throw err;
+        else {
+          switch (results[0].account_type) {
+            case "user":
+              res.render("user");
+              break;
+            case "admin":
+              res.render("admin");
+              break;
+            case "company":
+              res.render("company");
+              break;
+
+            default:
+              break;
+          }
+        }
       }
     );
   });
 });
+
 app.listen(3000, function () {
   console.log("server started!");
 });
